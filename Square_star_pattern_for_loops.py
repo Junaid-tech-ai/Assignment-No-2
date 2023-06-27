ff35b3985f2a1ec1cdad971d3000cff80bcb3e74
muhammadjunaid <engr.junaid001@gmail.com>
2023-06-27 20:55:21 +0500

    Initial commit

--- Square_star_pattern_for_loops.py
+++ Square_star_pattern_for_loops.py
@@ -11,11 +11,13 @@
 # 1 2 3 4
 # 1 2 3 4 5
 
+n=4 # n=4 is representing the number of rows
 for i in range(1,5):
     for j in range(1,11):
         print("*",end="")
     print()
 
+n=5 # n=5 is representing the number of rows
 for i in range(1,6):
     for j in range(1,i+1):
         print(j,end="")
