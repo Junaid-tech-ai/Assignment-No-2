e3eeb448e8d080fa9cb77a15e2cd8c0fe1d8f9a1
muhammadjunaid <engr.junaid001@gmail.com>
2023-06-27 21:29:41 +0500

    Initial commit

---
+++ program_to_clone_or_copy_list.py
@@ -0,0 +1,5 @@
+# Write a Python program to clone or copy a list.
+
+parentList=[5,6,"56","46"]
+cloneList=parentList.copy()
+print('The ParentList is =',parentList, "\n The clone list is =",cloneList)
