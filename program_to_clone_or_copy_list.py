68a8f911fe240f30855d842b2e3d93e7eefa2df1
muhammadjunaid <engr.junaid001@gmail.com>
2023-06-27 23:33:29 +0500

    implemented using for loops copy technique

--- program_to_clone_or_copy_list.py
+++ program_to_clone_or_copy_list.py
@@ -1,5 +1,7 @@
 # Write a Python program to clone or copy a list.
-
+newList=[]
 parentList=[5,6,"56","46"]
-cloneList=parentList.copy()
-print('The ParentList is =',parentList, "\n The clone list is =",cloneList)
+# cloneList=parentList.copy()
+for items in parentList:
+    newList.append(items)
+print(newList)
