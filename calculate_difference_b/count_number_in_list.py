2814c603eebc15c6fd5639292caf051d477209c8
muhammadjunaid <engr.junaid001@gmail.com>
2023-06-27 17:41:04 +0500

    Initial commit

---
+++ calculate_difference_b/count_number_in_list.py
@@ -0,0 +1,5 @@
+# Write a Python program to count the number 4 in a given list.
+numberList=[1,4,5,4,7,3,4,8,4,4,6,4,2,3,4,7,4]
+
+count_4=numberList.count(4)
+print("The count the number 4 in list is = ", count_4)
