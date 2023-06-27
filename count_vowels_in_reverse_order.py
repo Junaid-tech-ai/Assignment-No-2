df5f0193c7dea1a2abc53dac72f6e3d6e19c8559
muhammadjunaid <engr.junaid001@gmail.com>
2023-06-28 00:57:14 +0500

    Program to count vowels in reverse order

--- count_vowels_in_reverse_order.py
+++ count_vowels_in_reverse_order.py
@@ -1,15 +1,22 @@
 # Write a program which should count Vowels in the strings and return vowels in reverse order.
 string=str(input("Enter the String: \n"))
-count=0
-vowelList=[]
+
+
 def count_Vowels(string):
+    i=0
+    vowelList = []
+    count=0
     vowels=["a","e",'i',"o","u"]
     for character in string:
         if character in vowels:
             count +=1
             vowelList.append(character)
-    return count
-print(count)
+
+    for i in range(len(vowelList),0,-1):
+        print(vowelList[i-1])
+
+count_Vowels(string)
+
 
 
 
